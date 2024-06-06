100a6c81fb60de3971332a0df37090492c1a08d4
Rômulo <105829148+romulofranc@users.noreply.github.com>
2024-06-06 19:26:01 -0300

    Site completo

--- incluirEncomenda.js
+++ incluirEncomenda.js
@@ -1,85 +1,92 @@
 var botaoAdicionar = document.querySelector("#adicionar-encomenda");
 
-
 botaoAdicionar.addEventListener("click", function(event){
-   event.preventDefault();
-   console.log("Agora sim, o botão foi clicado!");
+
+    event.preventDefault();
+
+    //Captura o formulário da página
+    var form = document.querySelector("#form-adiciona");
+
+    //Captura os dados da nova encomenda
+    var encomenda = obtemEncomenda(form);
+
+    //Valida se a encomenda pode ser inserida
+    if(validaEncomenda(encomenda).length > 0) {
+        console.log(validaEncomenda(encomenda));
+    } else {
+        //Insere a nova encomenda na tabela
+        addEncomenda(encomenda);
+
+        //Limpa o formulário
+        form.reset();
+    }
+  
 });
 
-function Remove(){
-   document.querySelectorAll('.linhasTabela').forEach(function(linha){
-      linha.addEventListener('dblclick', function(){
-         console.log("Remover linha?")
-         var confirma = confirm("Tem certeza que deseja excluir a linha?")
-         if(confirma){
-            linha.remove();
-         }
-      });
+//Função para capturar os dados da nova encomenda
+function obtemEncomenda(dadosForm){
 
-   });
-   }
+    var encomenda = {
+        nome: dadosForm.nome.value,
+        qtde: dadosForm.qtde.value,
+        produto: dadosForm.produto.value,
+        unitario: dadosForm.unitario.value,
+    }
 
-   function RemoveNova(linha){
-
-         linha.addEventListener('dblclick', function(){
-            console.log("Remover Linha nova?")
-            var clica = confirm("Tem certeza que deseja excluir a linha?")
-            if(clica){
-               linha.remove();
-            }
-         });
-   
-   
-      };
-   
-
-function Nova(){
-   var Tabela = document.querySelector(".tabela");
-
-   //insere uma nova linha
-   var linha = Tabela.insertRow();
-
-   //inserir celula nas linhas
-   var celula1 = linha.insertCell(0);
-   var celula2 = linha.insertCell(1);
-   var celula3 = linha.insertCell(2);
-   var celula4 = linha.insertCell(3);
-   var celula5 = linha.insertCell(4);
-
-   var nome = document.querySelector(".idNome").value;
-   var prod = document.querySelector(".idProduto").value;
-   var qtd = parseInt(document.querySelector(".idQtd").value);
-   var unit = parseFloat(document.querySelector(".idUnitario").value);
-
-   celula1.textContent = nome;
-   celula2.textContent = prod;
-   celula3.textContent = qtd;
-   celula4.textContent = formata(unit);
-
-   RemoveNova(linha);
-   
-
-   if(validaQtd(qtd) == true){
-      celula3.textContent = "QTDE INVÁLIDA!";
-      celula3.style.color = "red";
-   }
-   else if(validaValor(unit) == true){
-      celula4.textContent = "UNITÁRIO INVÁLIDO!";
-      linha.style.backgroundColor = "red";
-   }
-   else{
-      celula5.textContent = formata(calculaTotal(qtd, unit));
-   }
-
-   if(nome.length <= 0){
-      celula1.textContent = "NOME NULO!"
-      linha.style.backgroundColor = "red";
-   }
-   if(prod == "Selecione"){
-      celula2.textContent = "PRODUTO INVÁLIDO";
-      celula2.style.color = "red";
-   }
-
+    return encomenda;
 }
 
-Remove(Nova);
+//Função para adicionar a nova encomenda na tabela
+function addEncomenda(novaEncomenda){
+
+    var tabela = document.querySelector("#tabelaInteira");
+
+    tabela.appendChild(montaTR(novaEncomenda));
+}
+
+//Monta uma coluna nova
+function montaTD(dado) {
+
+    var td = document.createElement("td");
+    td.textContent = dado;
+
+    return td;
+}
+
+//Monta uma nova TR
+function montaTR(novaEncomenda){
+
+    var tr = document.createElement("tr");
+    tr.classList.add("linhasTabela")
+
+    tr.appendChild(montaTD(novaEncomenda.nome));
+    tr.appendChild(montaTD(novaEncomenda.produto));
+    tr.appendChild(montaTD(novaEncomenda.qtde));
+    tr.appendChild(montaTD(formata(parseFloat(novaEncomenda.unitario))));
+    tr.appendChild(montaTD(formata(calculaTotal(novaEncomenda.qtde, novaEncomenda.unitario))));
+
+    return tr;
+}
+
+//Função para validação da quantidade e do unitário
+function validaEncomenda(encomenda){
+
+    var erros =[];
+
+    //Verifica se o nome foi informado
+    if(encomenda.nome=="") {
+        erros.push("O nome não pode ser vazio!");
+    }
+
+    //Verifica se a quantidade é maior que zero e um número
+    if(encomenda.qtde <= 0 || isNaN(encomenda.qtde)){
+        erros.push("A quantidade deve ser numérica e maior que 0.");
+    }
+
+    //Verifica se o valor unitário é maior que zero e um número
+    if(encomenda.unitario <=0 || isNaN(encomenda.unitario)){
+        erros.push("O valor unitário deve ser numérico e maior que 0.");
+    }
+
+    return erros;
+}
