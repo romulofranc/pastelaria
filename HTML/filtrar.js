2c2a71cd3efb56f1b70169d954e3eedf38ce983c
Rômulo <105829148+romulofranc@users.noreply.github.com>
2024-06-06 19:27:40 -0300

    site completo

---
+++ HTML/filtrar.js
@@ -0,0 +1,26 @@
+var campofiltro = document.querySelector('#filtrar-tabela');
+
+campofiltro.addEventListener("input", function(){
+    var clientes = document.querySelectorAll(".linhasTabela");
+
+    //Verifica se há um termo de busca
+    if(this.value.length>0){
+        for(var cli=0; cli<clientes.length; cli++){
+            var nome = clientes[cli].querySelector(".info-nome").textContent;
+            
+            var expressao = new RegExp(this.value, "i");
+
+            //Verifica os clientes para filtrar a tabela
+            //if(nome != this.value) {
+            if(!expressao.test(nome)){
+                clientes[cli].classList.add("invisivel");
+            }else{
+                clientes[cli].classList.remove("invisivel");
+            }
+        }
+    }else{
+        for(var cli=0; cli<clientes.length; cli++){
+            clientes[cli].classList.remove("invisivel");
+        }
+    }
+})
